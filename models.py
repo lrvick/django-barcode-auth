4ea8ab1427c35d61cd62a9c3888bb9575afa646e
Lance R. Vick <lance@lrvick.net>
2011-05-02 15:21:17 -0400

    initial model

--- models.py
+++ models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
+class UserBarcode(models.Model):
+    user = models.ForeignKey(User, unique=True)
+    barcode = models.ImageField(unique=True)
+
 # Create your models here.
