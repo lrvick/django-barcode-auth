69b8b25ec723799ce95b0d94e2bc397066cf0a07
Titus Soporan <titus@tsoporan.com>
2011-07-31 00:15:50 -0400

    fix duplicate card issues

--- utils.py
+++ utils.py
@@ -3,11 +3,12 @@ from django.contrib.auth.models import User
 from django.conf import settings
 
 try:
-    from reportlab.lib.units import inch
+    from reportlab.lib.units import toLength
     from reportlab.pdfgen import canvas
     from subprocess import Popen
-    import cStringIO
     from tempfile import NamedTemporaryFile
+    import os.path
+    import random
 except ImportError:
     canvas = False
 
@@ -23,34 +24,38 @@ def gen_passhash(user_id):
 
 
 def print_card(user, barcode):
-    PAGE_HEIGHT = 3.37 * inch
-    PAGE_WIDTH = 2.125 * inch
+    PAGE_HEIGHT = toLength('2.125in')
+    PAGE_WIDTH = toLength('3.37in')
     XCENTER = PAGE_WIDTH / 2.0
+    YCENTER = PAGE_HEIGHT / 2.0
     pdffile = NamedTemporaryFile()
     if user.first_name:
-        username = "%s %s" % (user.first_name, user.last_name)
+        username = '%s %s' % (user.first_name, user.last_name)
     else:
         username = user.username
-
     c = canvas.Canvas(pdffile.name, pagesize=(PAGE_WIDTH, PAGE_HEIGHT))
-    c.drawImage(barcode, 0.19 * inch, 0.1919 * inch, width=(1.75 * inch), height=(1.75 * inch), preserveAspectRatio=True)
-    c.setFont('Helvetica', 8)
-    c.drawCentredString(XCENTER, 2 * inch, settings.PRINT_MESSAGE_3)
-    c.setFont('Helvetica-Oblique', 8)
-    c.drawCentredString(XCENTER, 2.4 * inch, "Member since %s" % user.date_joined.strftime('%m/%Y'))
-    c.setFont('Helvetica-Oblique', 10)
-    c.drawCentredString(XCENTER, 2.6919 * inch, settings.PRINT_MESSAGE_2)
-    c.setFont('Helvetica-Bold', 14)
-    c.drawCentredString(XCENTER, 2.9 * inch, username)
-    c.setFont('Helvetica', 8)
-    c.drawCentredString(XCENTER, 3.11 * inch, settings.PRINT_MESSAGE_1)
+    if hasattr(settings, 'PRINT_CARD_IMAGES'):
+        for img in settings.PRINT_CARD_IMAGES:
+            c.drawImage(barcode if img['img'] else img['img'], toLength(img['x']), toLength(img['y']), width=(toLength(img['width'])), height=(toLength(img['height'])), preserveAspectRatio=True)
+    if hasattr(settings, 'PRINT_CARD_TEXT'):
+        
+        for msg in settings.PRINT_CARD_TEXT:
+            c.setFont(msg['font'], msg['size'])
+            c.drawCentredString(toLength(msg.get('x', XCENTER)), toLength(msg.get('y', YCENTER)), username if msg['text'] == '$username$' else msg['text'])
     c.showPage()
+    if hasattr(settings, 'PRINT_BACKS'):
+        if os.path.isdir(settings.PRINT_BACK_IMAGE):
+            back_image = '%s/%s' % (settings.PRINT_BACK_IMAGE, random.choice(os.listdir(settings.PRINT_BACK_IMAGE)))  # This is so ugly.
+        elif os.path.isfile(settings.PRINT_BACK_IMAGE):
+            back_image = settings.PRINT_BACK_IMAGE
+        c.drawImage(back_image, 0, 0, width=PAGE_WIDTH, height=PAGE_HEIGHT)
+        c.showPage()
     c.save()
-
     try:
-        printer = "-d %s" % settings.PRINTER
+        printer = '-d %s' % settings.PRINTER
     except NameError:
-        printer = ""
-
-    printjob = Popen("lp -s %s %s" % (printer, pdffile.name), shell=True)
+        printer = ''
+    
+    printjob = Popen('lp -s %s %s' % (printer, pdffile.name), shell=True)
     printjob.wait()
+
