f85dcae418990789df70d68a849bdf70ec74020c
David Pflug <david@tpflug.com>
2011-08-03 13:54:00 -0400

    Reimplementing dupe card fix

--- utils.py
+++ utils.py
@@ -6,7 +6,6 @@ try:
     from reportlab.lib.units import toLength
     from reportlab.pdfgen import canvas
     from subprocess import Popen
-    import cStringIO
     from tempfile import NamedTemporaryFile
     import os.path
     import random
@@ -14,7 +13,8 @@ except ImportError:
     canvas = False
 
 
-def gen_passhash(user):
+def gen_passhash(user_id):
+    user = User.objects.get(pk=user_id)
     token_handle = hashlib.sha224()
     token_handle.update(user.email)
     token_handle.update(user.password)
@@ -24,7 +24,6 @@ def gen_passhash(user):
 
 
 def print_card(user, barcode):
-    print(user)  # Don't remove this! For some reason, card printing starts using old data if you remove this. >.>; Also, it might be nice to figure out why this happens.
     PAGE_HEIGHT = toLength('2.125in')
     PAGE_WIDTH = toLength('3.37in')
     XCENTER = PAGE_WIDTH / 2.0
@@ -36,19 +35,18 @@ def print_card(user, barcode):
         username = user.username
 
     c = canvas.Canvas(pdffile.name, pagesize=(PAGE_WIDTH, PAGE_HEIGHT))
-    if settings.PRINT_CARD_IMAGES:
+
+    if hasattr(settings, 'PRINT_CARD_IMAGES'):
         for img in settings.PRINT_CARD_IMAGES:
-            if img['img'] == '$barcode$':
-                img['img'] = barcode
-            c.drawImage(img['img'], toLength(img['x']), toLength(img['y']), width=(toLength(img['width'])), height=(toLength(img['height'])), preserveAspectRatio=True)
-    if settings.PRINT_CARD_TEXT:
+            c.drawImage(barcode if img['img'] == '$barcode$' else img['img'], toLength(img['x']), toLength(img['y']), width=(toLength(img['width'])), height=(toLength(img['height'])), preserveAspectRatio=True)
+
+    if hasattr(settings, 'PRINT_CARD_TEXT'):
         for msg in settings.PRINT_CARD_TEXT:
-            if msg['text'] == '$username$':
-                msg['text'] = username
             c.setFont(msg['font'], msg['size'])
-            c.drawCentredString(toLength(msg.get('x', XCENTER)), toLength(msg.get('y', YCENTER)), msg['text'])
+            c.drawCentredString(toLength(msg.get('x', XCENTER)), toLength(msg.get('y', YCENTER)), username if msg['text'] == '$username$' else msg['text'])
+
     c.showPage()
-    if settings.PRINT_BACKS:
+    if hasattr(settings, 'PRINT_BACKS'):
         if os.path.isdir(settings.PRINT_BACK_IMAGE):
             back_image = '%s/%s' % (settings.PRINT_BACK_IMAGE, random.choice(os.listdir(settings.PRINT_BACK_IMAGE)))  # This is so ugly.
         elif os.path.isfile(settings.PRINT_BACK_IMAGE):
