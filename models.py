f726b646cbeeb11e051ceb7c53c33a395e200827
David Pflug <david@tpflug.com>
2011-09-26 13:49:37 -0400

    Always create barcodes on user save

--- models.py
+++ models.py
@@ -22,24 +22,23 @@ class UserBarcode(models.Model):
         return self.user.email
 
 def user_create_barcode(sender, instance, created, **kwargs):
-    if created:
-        instance = User.objects.get(email=instance.email)
-        password_hash = gen_passhash(instance)
-        qr = QRCode(8, QRErrorCorrectLevel.Q)
-        qr.addData("####%s|%s" % (str(instance.pk), str(password_hash)))
-        qr.make()
-        im = qr.makeImage()
-        temp_file = StringIO()
+    instance = User.objects.get(email=instance.email)
+    password_hash = gen_passhash(instance)
+    qr = QRCode(8, QRErrorCorrectLevel.Q)
+    qr.addData("####%s|%s" % (str(instance.pk), str(password_hash)))
+    qr.make()
+    im = qr.makeImage()
+    temp_file = StringIO()
 
-        # We'll take the username if we have to, but prefer first+last
-        im.save(temp_file, format='png')
-        barcode_contents = ContentFile(temp_file.getvalue())
+    # We'll take the username if we have to, but prefer first+last
+    im.save(temp_file, format='png')
+    barcode_contents = ContentFile(temp_file.getvalue())
 
-        user_barcode = UserBarcode(user=instance) 
-        user_barcode.barcode.save('%s.png' % str(instance.pk), barcode_contents)
+    user_barcode = UserBarcode(user=instance) 
+    user_barcode.barcode.save('%s.png' % str(instance.pk), barcode_contents)
 
-        if settings.PRINT_CARDS:
-            print_card(instance, user_barcode.barcode.name)
-        pass
+    if settings.PRINT_CARDS:
+        print_card(instance, user_barcode.barcode.name)
+    pass
 
 models.signals.post_save.connect(user_create_barcode, sender=User, dispatch_uid="apps.barauth.models")
